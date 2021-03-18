ba8bf50b4a7e254191bed0ae2896764794aa5e5a
Federico García <fede@divinodi.com>
2021-03-18 14:16:43 -0300

    Herramienta para importar pedidos de Shopify

---
+++ classes/ShopifyWebhook.php
@@ -0,0 +1,64 @@
+<?php
+
+class ShopifyWebhook {
+  static $db;
+
+  static function verify($signature, $secret, $data) {
+    $calculated_hmac = base64_encode(hash_hmac('sha256', $data, $secret, true));
+    return hash_equals($signature, $calculated_hmac);
+  }
+
+  static function connect($host, $user, $pwd, $name) {
+    $string = 'mysql:host=' . $host . ';dbname=' . $name . ';charset=utf8';
+    ShopifyWebhook::$db = new PDO($string, $user, $pwd, array(PDO::ATTR_PERSISTENT => false));
+  }
+
+  static function process($body) {
+    $data = array(
+      'id' => $body->id,
+      'createdAt' => $body->created_at,
+      'number' => $body->number,
+      'total' => $body->total_price,
+      'subtotal' => $body->subtotal_price,
+      'tax' => $body->total_tax,
+      'discounts' => $body->total_discounts,
+      'shipping' => static::shipping($body->shipping_lines),
+      'firstName' => $body->billing_address->first_name,
+      'lastName' => $body->billing_address->last_name,
+      'address' => trim($body->billing_address->address1 . ' ' . $body->billing_address->address2),
+      'phone' => $body->billing_address->phone,
+      'city' => $body->billing_address->city,
+      'province' => $body->billing_address->province,
+      'country' => $body->billing_address->country,
+      'email' => $body->customer->email,
+      'dni' => static::dni($body->note_attributes)
+    );
+
+    return ShopifyWebhook::insertOrder($data);
+  }
+
+  static function shipping($lines) {
+    return array_reduce($lines, function ($carry, $item) {
+      return $carry + $item->price;
+    }, 0);
+  }
+
+  static function dni($attributes) {
+    $attributes = array_filter($attributes, function ($att) {
+      return strtolower($att->name) === 'dni';
+    });
+
+    if (count($attributes))
+      return current($attributes)->value;
+    else
+      return null;
+  }
+
+  static function insertOrder($data) {
+    $fields = array_keys($data);
+    $placeholders = array_fill(0, count($fields), '?');
+
+    $query = 'INSERT INTO `orders` (' . implode(',', $fields) . ') VALUES (' . implode(',', $placeholders) . ')';
+    return static::$db->prepare($query)->execute(array_values($data));
+  }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/classes/ShopifyWebhook.php';
+
+$post = file_get_contents('php://input');
+if (!ShopifyWebhook::verify($_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'], WEBHOOK_SIGNATURE, $post))
+  die;
+
+ShopifyWebhook::connect(DBHOST, DBUSER, DBPASS, DBNAME);
+ShopifyWebhook::process(json_decode($post));
