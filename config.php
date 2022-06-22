3d4d6450768b3589e6d9cf9cdaedd703c04857bf
Federico García <fede@divinodi.com>
2022-06-22 17:22:12 -0300

    Agregar config con env vars, logs

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  define('WEBHOOK_SIGNATURE', getenv('WEBHOOK_SIGNATURE'));
+
+  define('DBHOST', getenv('DBHOST'));
+  define('DBUSER', getenv('DBUSER'));
+  define('DBPASS', getenv('DBPASS'));
+  define('DBNAME', getenv('DBNAME'));
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/classes/ShopifyWebhook.php';
 
 $post = file_get_contents('php://input');
+error_log("RECEIVED DATA: ".$post);
+
 if (!ShopifyWebhook::verify($_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'], WEBHOOK_SIGNATURE, $post))
   die;
 
